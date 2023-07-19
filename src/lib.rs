7d76be9fce8eabfdf35cfbdd5105fb15df6ea006
Simo Sorce <simo@redhat.com>
2023-07-19 16:46:15 -0400

    Test Library entry-point
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+mod interface {
+    #![allow(dead_code)]
+    include!("pkcs11_bindings.rs");
+}
+
+use interface::{CK_FUNCTION_LIST_PTR_PTR, CK_RV};
+
 #[no_mangle]
-pub extern "C" fn test() -> bool {
-    true
+pub extern "C" fn C_GetFunctionList(_ppFunctionList: CK_FUNCTION_LIST_PTR_PTR) -> CK_RV {
+    0
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn it_works() {
-        let result = test();
-        assert_eq!(result, true);
+        let list :CK_FUNCTION_LIST_PTR_PTR = std::ptr::null_mut();
+        let result = C_GetFunctionList(list);
+        assert_eq!(result, 0);
     }
 }
