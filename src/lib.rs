737c1f40ff905809e43e971ee73f0f3052219ec9
Simo Sorce <simo@redhat.com>
2023-07-20 14:55:44 -0400

    Add stubs for 3.0 interface
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -565,6 +565,303 @@ pub extern "C" fn C_GetFunctionList(fnlist: interface::CK_FUNCTION_LIST_PTR_PTR)
     CKR_OK
 }
 
+// Additional 3.0 functions
+
+extern "C" fn fn_login_user(
+        _session: interface::CK_SESSION_HANDLE,
+        _user_type: interface::CK_USER_TYPE,
+        _pin: interface::CK_UTF8CHAR_PTR,
+        _pin_len: interface::CK_ULONG,
+        _username: interface::CK_UTF8CHAR_PTR,
+        _username_len: interface::CK_ULONG,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_session_cancel(_session: interface::CK_SESSION_HANDLE, _flags: interface::CK_FLAGS) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_encrypt_init(
+        _session: interface::CK_SESSION_HANDLE,
+        _mechanism: interface::CK_MECHANISM_PTR,
+        _key: interface::CK_OBJECT_HANDLE,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_encrypt_message(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _associated_data: interface::CK_BYTE_PTR,
+        _associated_data_len: interface::CK_ULONG,
+        _plaintext: interface::CK_BYTE_PTR,
+        _plaintext_len: interface::CK_ULONG,
+        _ciphertext: interface::CK_BYTE_PTR,
+        _pul_ciphertext_len: interface::CK_ULONG_PTR,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_encrypt_message_begin(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _associated_data: interface::CK_BYTE_PTR,
+        _associated_data_len: interface::CK_ULONG,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_encrypt_message_next(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _plaintext_part: interface::CK_BYTE_PTR,
+        _plaintext_part_len: interface::CK_ULONG,
+        _ciphertext_part: interface::CK_BYTE_PTR,
+        _pul_ciphertext_part_len: interface::CK_ULONG_PTR,
+        _flags: interface::CK_FLAGS,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_encrypt_final(_session: interface::CK_SESSION_HANDLE) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_decrypt_init(
+        _session: interface::CK_SESSION_HANDLE,
+        _mechanism: interface::CK_MECHANISM_PTR,
+        _key: interface::CK_OBJECT_HANDLE,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_decrypt_message(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _associated_data: interface::CK_BYTE_PTR,
+        _associated_data_len: interface::CK_ULONG,
+        _ciphertext: interface::CK_BYTE_PTR,
+        _ciphertext_len: interface::CK_ULONG,
+        _plaintext: interface::CK_BYTE_PTR,
+        _pul_plaintext_len: interface::CK_ULONG_PTR,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_decrypt_message_begin(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _associated_data: interface::CK_BYTE_PTR,
+        _associated_data_len: interface::CK_ULONG,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_decrypt_message_next(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _ciphertext_part: interface::CK_BYTE_PTR,
+        _ciphertext_part_len: interface::CK_ULONG,
+        _plaintext_part: interface::CK_BYTE_PTR,
+        _pul_plaintext_part_len: interface::CK_ULONG_PTR,
+        _flags: interface::CK_FLAGS,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_decrypt_final(_session: interface::CK_SESSION_HANDLE) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_sign_init(
+        _session: interface::CK_SESSION_HANDLE,
+        _mechanism: interface::CK_MECHANISM_PTR,
+        _key: interface::CK_OBJECT_HANDLE,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_sign_message(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _data: interface::CK_BYTE_PTR,
+        _data_len: interface::CK_ULONG,
+        _signature: interface::CK_BYTE_PTR,
+        _pul_signature_len: interface::CK_ULONG_PTR,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_sign_message_begin(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_sign_message_next(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _data: interface::CK_BYTE_PTR,
+        _data_len: interface::CK_ULONG,
+        _signature: interface::CK_BYTE_PTR,
+        _pul_signature_len: interface::CK_ULONG_PTR,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_sign_final(_session: interface::CK_SESSION_HANDLE) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_verify_init(
+        _session: interface::CK_SESSION_HANDLE,
+        _mechanism: interface::CK_MECHANISM_PTR,
+        _key: interface::CK_OBJECT_HANDLE,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_verify_message(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _data: interface::CK_BYTE_PTR,
+        _data_len: interface::CK_ULONG,
+        _signature: interface::CK_BYTE_PTR,
+        _signature_len: interface::CK_ULONG,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_verify_message_begin(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_verify_message_next(
+        _session: interface::CK_SESSION_HANDLE,
+        _parameter: interface::CK_VOID_PTR,
+        _parameter_len: interface::CK_ULONG,
+        _data: interface::CK_BYTE_PTR,
+        _data_len: interface::CK_ULONG,
+        _signature: interface::CK_BYTE_PTR,
+        _signature_len: interface::CK_ULONG,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+extern "C" fn fn_message_verify_final(_session: interface::CK_SESSION_HANDLE) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+
+pub static mut FNLIST_300: interface::CK_FUNCTION_LIST_3_0 = interface::CK_FUNCTION_LIST_3_0 {
+    version: interface::CK_VERSION {
+        major: 3,
+        minor: 0},
+    C_Initialize: Some(fn_initialize),
+    C_Finalize: Some(fn_finalize),
+    C_GetInfo: Some(fn_get_info),
+    C_GetFunctionList: Some(C_GetFunctionList),
+    C_GetSlotList: Some(fn_get_slot_list),
+    C_GetSlotInfo: Some(fn_get_slot_info),
+    C_GetTokenInfo: Some(fn_get_token_info),
+    C_GetMechanismList: Some(fn_get_mechanism_list),
+    C_GetMechanismInfo: Some(fn_get_mechanism_info),
+    C_InitToken: Some(fn_init_token),
+    C_InitPIN: Some(fn_init_pin),
+    C_SetPIN: Some(fn_set_pin),
+    C_OpenSession: Some(fn_open_session),
+    C_CloseSession: Some(fn_close_session),
+    C_CloseAllSessions: Some(fn_close_all_sessions),
+    C_GetSessionInfo: Some(fn_get_session_info),
+    C_GetOperationState: Some(fn_get_operation_state),
+    C_SetOperationState: Some(fn_set_operation_state),
+    C_Login: Some(fn_login),
+    C_Logout: Some(fn_logout),
+    C_CreateObject: Some(fn_create_object),
+    C_CopyObject: Some(fn_copy_object),
+    C_DestroyObject: Some(fn_destroy_object),
+    C_GetObjectSize: Some(fn_get_object_size),
+    C_GetAttributeValue: Some(fn_get_attribute_value),
+    C_SetAttributeValue: Some(fn_set_attribute_value),
+    C_FindObjectsInit: Some(fn_find_objects_init),
+    C_FindObjects: Some(fn_find_objects),
+    C_FindObjectsFinal: Some(fn_find_objects_final),
+    C_EncryptInit: Some(fn_encrypt_init),
+    C_Encrypt: Some(fn_encrypt),
+    C_EncryptUpdate: Some(fn_encrypt_update),
+    C_EncryptFinal: Some(fn_encrypt_final),
+    C_DecryptInit: Some(fn_decrypt_init),
+    C_Decrypt: Some(fn_decrypt),
+    C_DecryptUpdate: Some(fn_decrypt_update),
+    C_DecryptFinal: Some(fn_decrypt_final),
+    C_DigestInit: Some(fn_digest_init),
+    C_Digest: Some(fn_digest),
+    C_DigestUpdate: Some(fn_digest_update),
+    C_DigestKey: Some(fn_digest_key),
+    C_DigestFinal: Some(fn_digest_final),
+    C_SignInit: Some(fn_sign_init),
+    C_Sign: Some(fn_sign),
+    C_SignUpdate: Some(fn_sign_update),
+    C_SignFinal: Some(fn_sign_final),
+    C_SignRecoverInit: Some(fn_sign_recover_init),
+    C_SignRecover: Some(fn_sign_recover),
+    C_VerifyInit: Some(fn_verify_init),
+    C_Verify: Some(fn_verify),
+    C_VerifyUpdate: Some(fn_verify_update),
+    C_VerifyFinal: Some(fn_verify_final),
+    C_VerifyRecoverInit: Some(fn_verify_recover_init),
+    C_VerifyRecover: Some(fn_verify_recover),
+    C_DigestEncryptUpdate: Some(fn_digest_encrypt_update),
+    C_DecryptDigestUpdate: Some(fn_decrypt_digest_update),
+    C_SignEncryptUpdate: Some(fn_sign_encrypt_update),
+    C_DecryptVerifyUpdate: Some(fn_decrypt_verify_update),
+    C_GenerateKey: Some(fn_generate_key),
+    C_GenerateKeyPair: Some(fn_generate_key_pair),
+    C_WrapKey: Some(fn_wrap_key),
+    C_UnwrapKey: Some(fn_unwrap_key),
+    C_DeriveKey: Some(fn_derive_key),
+    C_SeedRandom: Some(fn_seed_random),
+    C_GenerateRandom: Some(fn_generate_random),
+    C_GetFunctionStatus: Some(fn_get_function_status),
+    C_CancelFunction: Some(fn_cancel_function),
+    C_WaitForSlotEvent: Some(fn_wait_for_slot_event),
+    C_GetInterfaceList: Some(C_GetInterfaceList),
+    C_GetInterface: Some(C_GetInterface),
+    C_LoginUser: Some(fn_login_user),
+    C_SessionCancel: Some(fn_session_cancel),
+    C_MessageEncryptInit: Some(fn_message_encrypt_init),
+    C_EncryptMessage: Some(fn_encrypt_message),
+    C_EncryptMessageBegin: Some(fn_encrypt_message_begin),
+    C_EncryptMessageNext: Some(fn_encrypt_message_next),
+    C_MessageEncryptFinal: Some(fn_message_encrypt_final),
+    C_MessageDecryptInit: Some(fn_message_decrypt_init),
+    C_DecryptMessage: Some(fn_decrypt_message),
+    C_DecryptMessageBegin: Some(fn_decrypt_message_begin),
+    C_DecryptMessageNext: Some(fn_decrypt_message_next),
+    C_MessageDecryptFinal: Some(fn_message_decrypt_final),
+    C_MessageSignInit: Some(fn_message_sign_init),
+    C_SignMessage: Some(fn_sign_message),
+    C_SignMessageBegin: Some(fn_sign_message_begin),
+    C_SignMessageNext: Some(fn_sign_message_next),
+    C_MessageSignFinal: Some(fn_message_sign_final),
+    C_MessageVerifyInit: Some(fn_message_verify_init),
+    C_VerifyMessage: Some(fn_verify_message),
+    C_VerifyMessageBegin: Some(fn_verify_message_begin),
+    C_VerifyMessageNext: Some(fn_verify_message_next),
+    C_MessageVerifyFinal: Some(fn_message_verify_final),
+};
+
+#[no_mangle]
+pub extern "C" fn C_GetInterfaceList(
+        _interfaces_list: interface::CK_INTERFACE_PTR,
+        _pul_count: interface::CK_ULONG_PTR,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
+
+#[no_mangle]
+pub extern "C" fn C_GetInterface(
+        _interface_name: interface::CK_UTF8CHAR_PTR,
+        _version: interface::CK_VERSION_PTR,
+        _interface: interface::CK_INTERFACE_PTR_PTR,
+        _flags: interface::CK_FLAGS,
+    ) -> CK_RV {
+    CKR_FUNCTION_NOT_SUPPORTED
+}
 
 #[cfg(test)]
 mod tests {
