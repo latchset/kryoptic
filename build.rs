1a977a4e34b0ebccba079d18a2c108cdd71a3e42
Simo Sorce <simo@redhat.com>
2024-02-19 15:36:52 -0500

    Simplify build.rs OpenSSL/FIPs targets
    Remove duplication
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- build.rs
+++ build.rs
@@ -31,35 +31,48 @@ impl bindgen::callbacks::ParseCallbacks for Pkcs11Callbacks {
     }
 }
 
-#[cfg(feature = "fips")]
-fn build_fips() {
+fn build_ossl() {
     let openssl_path = std::path::PathBuf::from("openssl")
         .canonicalize()
         .expect("cannot canonicalize path");
 
-    let providers_path = openssl_path
-        .join("providers")
-        .canonicalize()
-        .expect("OpenSSL providers path unavailable");
+    #[cfg(feature = "fips")]
+    let (libpath, bldargs) = {
+        let providers_path = openssl_path
+            .join("providers")
+            .canonicalize()
+            .expect("OpenSSL providers path unavailable");
 
-    println!(
-        "cargo:rustc-link-search={}",
-        providers_path.to_str().unwrap()
-    );
-    println!("cargo:rustc-link-lib=static=fips");
+        println!(
+            "cargo:rustc-link-search={}",
+            providers_path.to_str().unwrap()
+        );
+        println!("cargo:rustc-link-lib=static=fips");
+        (
+            format!("{}/libfips.a", providers_path.to_str().unwrap())
+                .to_string(),
+            ["--debug", "enable-fips"],
+        )
+    };
 
-    match std::path::Path::new(
-        format!("{}/libfips.a", providers_path.to_str().unwrap()).as_str(),
-    )
-    .try_exists()
-    {
+    #[cfg(not(feature = "fips"))]
+    let (libpath, bldargs) = {
+        println!("cargo:rustc-link-search={}", openssl_path.to_str().unwrap());
+        println!("cargo:rustc-link-lib=static=crypto");
+        (
+            format!("{}/libcrypto.a", openssl_path.to_str().unwrap())
+                .to_string(),
+            ["--debug"],
+        )
+    };
+
+    match std::path::Path::new(&libpath).try_exists() {
         Ok(true) => (),
         _ => {
             /* openssl: ./Configure --debug enable-fips */
             if !std::process::Command::new("./Configure")
                 .current_dir(&openssl_path)
-                .arg("--debug")
-                .arg("enable-fips")
+                .args(bldargs)
                 .output()
                 .expect("could not run openssl `Configure`")
                 .status
@@ -102,77 +115,7 @@ fn build_fips() {
         .allowlist_item("EVP_.*")
         .allowlist_item("evp_.*")
         .allowlist_item("BN_.*")
-        .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file("src/fips/bindings.rs")
-        .expect("Couldn't write bindings!");
-}
-
-#[cfg(not(feature = "fips"))]
-fn build_ossl() {
-    let openssl_path = std::path::PathBuf::from("openssl")
-        .canonicalize()
-        .expect("cannot canonicalize path");
-
-    println!(
-        "cargo:rustc-link-search={}",
-        openssl_path.to_str().unwrap()
-    );
-    println!("cargo:rustc-link-lib=static=crypto");
-
-    match std::path::Path::new(
-        format!("{}/libcrypto.a", openssl_path.to_str().unwrap()).as_str(),
-    )
-    .try_exists()
-    {
-        Ok(true) => (),
-        _ => {
-            /* openssl: ./Configure --debug enable-fips */
-            if !std::process::Command::new("./Configure")
-                .current_dir(&openssl_path)
-                .arg("--debug")
-                .output()
-                .expect("could not run openssl `Configure`")
-                .status
-                .success()
-            {
-                // Panic if the command was not successful.
-                panic!("could not configure OpenSSL");
-            }
-
-            if !std::process::Command::new("make")
-                .current_dir(&openssl_path)
-                .output()
-                .expect("could not run openssl `make`")
-                .status
-                .success()
-            {
-                // Panic if the command was not successful.
-                panic!("could not build OpenSSL");
-            }
-        }
-    }
-
-    let include_path = openssl_path
-        .join("include")
-        .canonicalize()
-        .expect("OpenSSL include path unavailable");
-
-    bindgen::Builder::default()
-        .header("ossl.h")
-        .clang_arg(format!("-I{}", include_path.to_str().unwrap()))
-        .clang_arg("-std=c90") /* workaround [-Wimplicit-int] */
-        .derive_default(true)
-        .formatter(bindgen::Formatter::Prettyplease)
-        .allowlist_item("ossl_.*")
-        .allowlist_item("OSSL_.*")
-        .allowlist_item("openssl_.*")
-        .allowlist_item("OPENSSL_.*")
-        .allowlist_item("CRYPTO_.*")
-        .allowlist_item("c_.*")
-        .allowlist_item("EVP_.*")
-        .allowlist_item("evp_.*")
-        .allowlist_item("BN_.*")
+        .allowlist_item("LN_aes.*")
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("src/ossl/bindings.rs")
@@ -194,11 +137,6 @@ fn main() {
         .write_to_file("src/pkcs11/bindings.rs")
         .expect("Couldn't write bindings!");
 
-    /* OpenSSL FIPS provider */
-    #[cfg(feature = "fips")]
-    build_fips();
-
     /* OpenSSL Cryptography */
-    #[cfg(not(feature = "fips"))]
     build_ossl();
 }
