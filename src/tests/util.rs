9957845ef47c885d5eb440c937cc050ca8943278
Simo Sorce <simo@redhat.com>
2024-05-09 17:19:07 -0400

    Move more utility functions in util.rs
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/tests/util.rs
+++ src/tests/util.rs
@@ -25,3 +25,296 @@ macro_rules! ret_or_panic {
         }
     };
 }
+
+pub fn get_test_data(
+    session: CK_SESSION_HANDLE,
+    name: &str,
+    data: &str,
+) -> Result<Vec<u8>, CK_RV> {
+    let mut handle: CK_ULONG = CK_INVALID_HANDLE;
+    let mut template = vec![
+        make_attribute!(
+            CKA_APPLICATION,
+            CString::new(name).unwrap().into_raw(),
+            name.len()
+        ),
+        make_attribute!(
+            CKA_LABEL,
+            CString::new(data).unwrap().into_raw(),
+            data.len()
+        ),
+    ];
+    let mut ret = fn_find_objects_init(
+        session,
+        template.as_mut_ptr(),
+        template.len() as CK_ULONG,
+    );
+    if ret != CKR_OK {
+        return Err(ret);
+    }
+    let mut count: CK_ULONG = 0;
+    ret = fn_find_objects(session, &mut handle, 1, &mut count);
+    if ret != CKR_OK {
+        return Err(ret);
+    }
+    ret = fn_find_objects_final(session);
+    if ret != CKR_OK {
+        return Err(ret);
+    }
+
+    /* get value */
+    template.clear();
+    template.push(make_attribute!(CKA_VALUE, std::ptr::null_mut(), 0));
+    ret = fn_get_attribute_value(
+        session,
+        handle,
+        template.as_mut_ptr(),
+        template.len() as CK_ULONG,
+    );
+    if ret != CKR_OK {
+        return Err(ret);
+    }
+
+    let mut value = vec![0; template[0].ulValueLen as usize];
+    template[0].pValue = value.as_mut_ptr() as CK_VOID_PTR;
+    ret = fn_get_attribute_value(
+        session,
+        handle,
+        template.as_mut_ptr(),
+        template.len() as CK_ULONG,
+    );
+    if ret != CKR_OK {
+        return Err(ret);
+    }
+
+    Ok(value)
+}
+
+pub fn decrypt(
+    session: CK_SESSION_HANDLE,
+    key: CK_OBJECT_HANDLE,
+    ciphertext: &Vec<u8>,
+    mechanism: &CK_MECHANISM,
+) -> KResult<Vec<u8>> {
+    let ret = fn_decrypt_init(
+        session,
+        mechanism as *const _ as CK_MECHANISM_PTR,
+        key,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    let mut dec_len: CK_ULONG = 0;
+    let ret = fn_decrypt(
+        session,
+        ciphertext.as_ptr() as *mut u8,
+        ciphertext.len() as CK_ULONG,
+        std::ptr::null_mut(),
+        &mut dec_len,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    let mut dec = vec![0u8; dec_len as usize];
+    let ret = fn_decrypt(
+        session,
+        ciphertext.as_ptr() as *mut u8,
+        ciphertext.len() as CK_ULONG,
+        dec.as_mut_ptr(),
+        &mut dec_len,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    dec.resize(dec_len as usize, 0);
+
+    Ok(dec)
+}
+
+pub fn encrypt(
+    session: CK_SESSION_HANDLE,
+    key: CK_OBJECT_HANDLE,
+    plaintext: &Vec<u8>,
+    mechanism: &CK_MECHANISM,
+) -> KResult<Vec<u8>> {
+    let ret = fn_encrypt_init(
+        session,
+        mechanism as *const _ as CK_MECHANISM_PTR,
+        key,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    let mut enc_len: CK_ULONG = 0;
+    let ret = fn_encrypt(
+        session,
+        plaintext.as_ptr() as *mut u8,
+        plaintext.len() as CK_ULONG,
+        std::ptr::null_mut(),
+        &mut enc_len,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    let mut enc = vec![0u8; enc_len as usize];
+    let ret = fn_encrypt(
+        session,
+        plaintext.as_ptr() as *mut u8,
+        plaintext.len() as CK_ULONG,
+        enc.as_mut_ptr(),
+        &mut enc_len,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    enc.resize(enc_len as usize, 0);
+
+    Ok(enc)
+}
+
+/* name in CKA_ID */
+pub fn get_test_key_handle(
+    session: CK_SESSION_HANDLE,
+    name: &str,
+    class: CK_ULONG,
+) -> KResult<CK_OBJECT_HANDLE> {
+    let mut handle: CK_ULONG = CK_INVALID_HANDLE;
+    let mut classbuf = class;
+    let mut template = vec![
+        make_attribute!(
+            CKA_ID,
+            CString::new(name).unwrap().into_raw(),
+            name.len()
+        ),
+        make_attribute!(CKA_CLASS, &mut classbuf as *mut _, CK_ULONG_SIZE),
+    ];
+    let ret = fn_find_objects_init(session, template.as_mut_ptr(), 2);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    let mut count: CK_ULONG = 0;
+    let ret = fn_find_objects(session, &mut handle, 1, &mut count);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    if count != 1 {
+        return err_not_found!(format!("count {} != 1", count));
+    }
+    let ret = fn_find_objects_final(session);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    Ok(handle)
+}
+
+pub fn sig_verify(
+    session: CK_SESSION_HANDLE,
+    key: CK_OBJECT_HANDLE,
+    data: &Vec<u8>,
+    signature: &Vec<u8>,
+    mechanism: &CK_MECHANISM,
+) -> CK_RV {
+    let ret =
+        fn_verify_init(session, mechanism as *const _ as CK_MECHANISM_PTR, key);
+    if ret != CKR_OK {
+        return ret;
+    }
+
+    fn_verify(
+        session,
+        data.as_ptr() as *mut u8,
+        data.len() as CK_ULONG,
+        signature.as_ptr() as *mut u8,
+        signature.len() as CK_ULONG,
+    )
+}
+
+pub fn sig_gen(
+    session: CK_SESSION_HANDLE,
+    key: CK_OBJECT_HANDLE,
+    data: &Vec<u8>,
+    mechanism: &CK_MECHANISM,
+) -> KResult<Vec<u8>> {
+    let ret =
+        fn_sign_init(session, mechanism as *const _ as CK_MECHANISM_PTR, key);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    /* get signature length */
+    let mut siglen: CK_ULONG = 0;
+    let ret = fn_sign(
+        session,
+        data.as_ptr() as *mut u8,
+        data.len() as CK_ULONG,
+        std::ptr::null_mut(),
+        &mut siglen,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    let mut signature: Vec<u8> = vec![0; siglen as usize];
+    let ret = fn_sign(
+        session,
+        data.as_ptr() as *mut u8,
+        data.len() as CK_ULONG,
+        signature.as_ptr() as *mut u8,
+        &mut siglen,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    signature.resize(siglen as usize, 0);
+
+    Ok(signature)
+}
+
+pub fn sig_gen_multipart(
+    session: CK_SESSION_HANDLE,
+    key: CK_OBJECT_HANDLE,
+    data: &Vec<u8>,
+    mechanism: &CK_MECHANISM,
+) -> KResult<Vec<u8>> {
+    let ret =
+        fn_sign_init(session, mechanism as *const _ as CK_MECHANISM_PTR, key);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    let half_len = data.len() / 2;
+    // just send data in two chunks
+    let ret =
+        fn_sign_update(session, data.as_ptr() as *mut u8, half_len as CK_ULONG);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    let ret = fn_sign_update(
+        session,
+        data[half_len..].as_ptr() as *mut u8,
+        (data.len() - half_len) as CK_ULONG,
+    );
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    let mut siglen: CK_ULONG = 0;
+    let ret = fn_sign_final(session, std::ptr::null_mut(), &mut siglen);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+
+    let mut signature: Vec<u8> = vec![0; siglen as usize];
+    let ret =
+        fn_sign_final(session, signature.as_ptr() as *mut u8, &mut siglen);
+    if ret != CKR_OK {
+        return err_rv!(ret);
+    }
+    signature.resize(siglen as usize, 0);
+
+    Ok(signature)
+}
