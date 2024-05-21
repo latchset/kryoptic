51a9554cfb983ae21e3668b0de419c43a049f36e
Jakub Jelen <jjelen@redhat.com>
2024-05-21 10:07:16 +0200

    Fix spellcheck for commit messages
    It was failing in CI as
    
    fatal: ambiguous argument 'main..': unknown revision or path not in the working tree.
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- .github/codespell.sh
+++ .github/codespell.sh
@@ -7,7 +7,7 @@ echo "Running codespell on source code..."
 $CODESPELL --skip **/bindings.rs src || result=1
 
 # assuming the main branch is there
-for COMMIT in $(git rev-list main..); do
+for COMMIT in $(git rev-list main.. --); do
 	echo "Running codespell on commit message of $COMMIT..."
 	git show --format=%B -s "$COMMIT" | $CODESPELL - || result=1
 done
