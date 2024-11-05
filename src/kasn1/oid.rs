4aab2145667e9b42c7147af2cb0c5a9f8244af2a
Simo Sorce <simo@redhat.com>
2024-11-05 15:11:17 -0500

    Add support for checking signatures on attributes
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/kasn1/oid.rs
+++ src/kasn1/oid.rs
@@ -1,3 +1,6 @@
 // Copyright 2024 Simo Sorce
 // See LICENSE.txt file for terms
 include! {"pyca/oid.rs"}
+
+pub const PBMAC1_OID: asn1::ObjectIdentifier =
+    asn1::oid!(1, 2, 840, 113549, 1, 5, 14);
