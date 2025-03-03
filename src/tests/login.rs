6b6eb9fcac1b4ecb213be4c687340f60bf80880e
Jakub Jelen <jjelen@redhat.com>
2025-03-03 19:29:15 +0100

    tests: Reproducer for #163
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/tests/login.rs
+++ src/tests/login.rs
@@ -138,3 +138,41 @@ fn test_login() {
 
     testtokn.finalize();
 }
+
+#[test]
+#[parallel]
+fn test_login_close() {
+    let mut testtokn = TestToken::initialized("test_login_close", None);
+
+    /* Run this twice and make sure the second call does not return ALREADY_LOGGED_IN */
+    for _ in 0..2 {
+        let session = testtokn.get_session(true);
+
+        let mut info = CK_SESSION_INFO {
+            slotID: CK_UNAVAILABLE_INFORMATION,
+            state: CK_UNAVAILABLE_INFORMATION,
+            flags: 0,
+            ulDeviceError: 0,
+        };
+        let ret = fn_get_session_info(session, &mut info);
+        assert_eq!(ret, CKR_OK);
+        assert_eq!(info.state, CKS_RW_PUBLIC_SESSION);
+
+        /* login */
+        let pin = "12345678";
+        let ret = fn_login(
+            session,
+            CKU_USER,
+            pin.as_ptr() as *mut _,
+            pin.len() as CK_ULONG,
+        );
+        assert_eq!(ret, CKR_OK);
+
+        let ret = fn_get_session_info(session, &mut info);
+        assert_eq!(ret, CKR_OK);
+        assert_eq!(info.state, CKS_RW_USER_FUNCTIONS);
+
+        /* close session should reset the login state */
+        testtokn.close_session();
+    }
+}
