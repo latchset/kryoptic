0d4751988bef134caa0687e74ccb257e3f04cd1c
Simo Sorce <simo@redhat.com>
2025-04-30 18:46:28 -0400

    FIPS module doc strings
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/fips/mod.rs
+++ src/fips/mod.rs
@@ -11,18 +11,22 @@ use crate::token::Token;
 
 pub(crate) mod indicators;
 
+/// Sets the FIPS module into the error state
 pub fn set_fips_error_state() {
     fips::set_error_state();
 }
 
+/// Checks if the FIPS module is in an error state
 pub fn check_fips_state_ok() -> bool {
     return fips::check_state_ok();
 }
 
+/// Helper function to set up validation objects at token initialization
 pub fn token_init(token: &mut Token) -> Result<()> {
     indicators::insert_fips_validation(token)
 }
 
+/// Helper function to register the validation object factory
 pub fn register(_: &mut Mechanisms, ot: &mut ObjectFactories) {
     ot.add_factory(
         ObjectType::new(CKO_VALIDATION, 0),
@@ -30,6 +34,8 @@ pub fn register(_: &mut Mechanisms, ot: &mut ObjectFactories) {
     );
 }
 
+/// Check a key template and based on the requested `FipsBehavior`
+/// checks whether the CKA_SENSITIVE attribute contains an appropriate value
 pub fn check_key_template(
     template: &[CK_ATTRIBUTE],
     fips_opts: FipsBehavior,
