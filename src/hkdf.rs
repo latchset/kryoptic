022e8fd2837bc12cae36bddd8c2f8ec9cffaafaf
Simo Sorce <simo@redhat.com>
2025-05-01 09:26:44 -0400

    Hkdf module doc strings
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/hkdf.rs
+++ src/hkdf.rs
@@ -1,6 +1,10 @@
 // Copyright 2024 Simo Sorce
 // See LICENSE.txt file for terms
 
+//! This module implements the PKCS#11 mechanisms to access the HMAC-based
+//! Key Derivation Function (HKDF) as specified in
+//! [RFC 5869](https://www.rfc-editor.org/rfc/rfc5869)
+
 use std::fmt::Debug;
 
 use crate::error::Result;
@@ -9,16 +13,19 @@ use crate::mechanism::{Derive, Mechanism, Mechanisms};
 use crate::object::{GenericSecretKeyMechanism, ObjectFactories};
 use crate::ossl::hkdf::HKDFOperation;
 
+/// Registers all HKDF related mechanisms
 pub fn register(mechs: &mut Mechanisms, _: &mut ObjectFactories) {
     HKDFMechanism::register_mechanisms(mechs);
 }
 
+/// Object that represents the HKDF mechanism
 #[derive(Debug)]
 struct HKDFMechanism {
     info: CK_MECHANISM_INFO,
 }
 
 impl HKDFMechanism {
+    /// Instantiates and registers the HKDF related mechanisms
     fn register_mechanisms(mechs: &mut Mechanisms) {
         mechs.add_mechanism(
             CKM_HKDF_DERIVE,
@@ -48,10 +55,12 @@ impl HKDFMechanism {
 }
 
 impl Mechanism for HKDFMechanism {
+    /// Returns a reference to the mechanism info
     fn info(&self) -> &CK_MECHANISM_INFO {
         &self.info
     }
 
+    /// Initializes a HKDF Derive operation
     fn derive_operation(&self, mech: &CK_MECHANISM) -> Result<Box<dyn Derive>> {
         if self.info.flags & CKF_DERIVE != CKF_DERIVE {
             return Err(CKR_MECHANISM_INVALID)?;
