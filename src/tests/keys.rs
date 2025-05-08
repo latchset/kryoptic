aeb01927fdbefa65261c6c68c7821f5f8f5ea84a
Simo Sorce <simo@redhat.com>
2025-05-08 11:21:12 -0400

    Test key-gen and key-wrapping for all EC key types
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/tests/keys.rs
+++ src/tests/keys.rs
@@ -354,69 +354,64 @@ fn test_rsa_key() {
     testtokn.finalize();
 }
 
-#[cfg(feature = "ecdsa")]
+#[cfg(any(feature = "ecdsa", feature = "eddsa", feature = "ec_montgomery"))]
 #[test]
 #[parallel]
-fn test_ecc_key() {
-    let mut testtokn = TestToken::initialized("test_ecc_key", None);
+fn test_ec_keys() {
+    let mut testtokn = TestToken::initialized("test_ec_keys", None);
     let session = testtokn.get_session(true);
 
     /* login */
     testtokn.login();
 
-    /* EC key pair */
-    let ec_params = hex::decode(
-        "06052B81040022", // secp384r1
-    )
-    .expect("Failed to decode hex ec_params");
+    struct TestData {
+        ec_params: Vec<u8>,
+        key_type: CK_KEY_TYPE,
+        key_gen: CK_MECHANISM_TYPE,
+        op_flags: (CK_ATTRIBUTE_TYPE, CK_ATTRIBUTE_TYPE),
+        op_alg: CK_MECHANISM_TYPE,
+        out_size: usize,
+    }
 
-    let (pubkey, prikey) = ret_or_panic!(generate_key_pair(
-        session,
-        CKM_EC_KEY_PAIR_GEN,
-        &[(CKA_CLASS, CKO_PUBLIC_KEY), (CKA_KEY_TYPE, CKK_EC),],
-        &[(CKA_EC_PARAMS, ec_params.as_slice())],
-        &[(CKA_VERIFY, true)],
-        &[(CKA_CLASS, CKO_PRIVATE_KEY), (CKA_KEY_TYPE, CKK_EC),],
-        &[],
-        &[
-            (CKA_PRIVATE, true),
-            (CKA_SENSITIVE, true),
-            (CKA_TOKEN, true),
-            (CKA_SIGN, true),
-            (CKA_EXTRACTABLE, true),
-        ],
-    ));
-
-    let data = "plaintext";
-    let sig = ret_or_panic!(sig_gen(
-        session,
-        prikey,
-        data.as_bytes(),
-        &CK_MECHANISM {
-            mechanism: CKM_ECDSA_SHA256,
-            pParameter: std::ptr::null_mut(),
-            ulParameterLen: 0,
-        },
-    ));
-    assert_eq!(sig.len(), 96);
-
-    assert_eq!(
-        CKR_OK,
-        sig_verify(
-            session,
-            pubkey,
-            data.as_bytes(),
-            sig.as_slice(),
-            &CK_MECHANISM {
-                mechanism: CKM_ECDSA_SHA256,
-                pParameter: std::ptr::null_mut(),
-                ulParameterLen: 0,
-            },
+    let mut testdata = Vec::<TestData>::new();
+    #[cfg(feature = "ecdsa")]
+    testdata.push(TestData {
+        ec_params: hex::decode(
+            "06052B81040022", // secp384r1
         )
-    );
+        .expect("Failed to decode hex ec_params"),
+        key_type: CKK_EC,
+        key_gen: CKM_EC_KEY_PAIR_GEN,
+        op_flags: (CKA_VERIFY, CKA_SIGN),
+        op_alg: CKM_ECDSA_SHA256,
+        out_size: 96,
+    });
+    #[cfg(feature = "eddsa")]
+    testdata.push(TestData {
+        ec_params: hex::decode(
+            "130c656477617264733235353139", // edwards25519
+        )
+        .expect("Failed to decode hex ec_params"),
+        key_type: CKK_EC_EDWARDS,
+        key_gen: CKM_EC_EDWARDS_KEY_PAIR_GEN,
+        op_flags: (CKA_VERIFY, CKA_SIGN),
+        op_alg: CKM_EDDSA,
+        out_size: 64,
+    });
+    #[cfg(feature = "ec_montgomery")]
+    testdata.push(TestData {
+        ec_params: hex::decode("130a63757276653235353139")
+            .expect("Failed to decode hex ec_params"),
+        key_type: CKK_EC_MONTGOMERY,
+        key_gen: CKM_EC_MONTGOMERY_KEY_PAIR_GEN,
+        op_flags: (CKA_DERIVE, CKA_DERIVE),
+        op_alg: CK_UNAVAILABLE_INFORMATION,
+        out_size: 0,
+    });
+    let data = "plaintext";
 
-    /* Wrap EC key in AES */
-    let handle = ret_or_panic!(generate_key(
+    /* Key for wrapping tests */
+    let wk_handle = ret_or_panic!(generate_key(
         session,
         CKM_AES_KEY_GEN,
         std::ptr::null_mut(),
@@ -431,87 +426,143 @@ fn test_ecc_key() {
         ],
     ));
 
-    let iv = [0xCCu8; 4];
-    let mut mechanism: CK_MECHANISM = CK_MECHANISM {
-        mechanism: CKM_AES_KEY_WRAP_KWP,
-        pParameter: void_ptr!(&iv),
-        ulParameterLen: iv.len() as CK_ULONG,
-    };
-
-    let mut wrapped = vec![0u8; 65536];
-    let mut wrapped_len = wrapped.len() as CK_ULONG;
-
-    let mut ret = fn_wrap_key(
-        session,
-        &mut mechanism,
-        handle,
-        prikey,
-        wrapped.as_mut_ptr(),
-        &mut wrapped_len,
-    );
-    assert_eq!(ret, CKR_OK);
-
-    assert_eq!(check_validation(session, 1), true);
-
-    let mut pri_template = make_attr_template(
-        &[(CKA_CLASS, CKO_PRIVATE_KEY), (CKA_KEY_TYPE, CKK_EC)],
-        &[],
-        &[
-            (CKA_PRIVATE, true),
-            (CKA_SENSITIVE, true),
-            (CKA_TOKEN, true),
-            (CKA_SIGN, true),
-            (CKA_EXTRACTABLE, true),
-        ],
-    );
-
-    let mut prikey2 = CK_INVALID_HANDLE;
-    ret = fn_unwrap_key(
-        session,
-        &mut mechanism,
-        handle,
-        wrapped.as_mut_ptr(),
-        wrapped_len,
-        pri_template.as_mut_ptr(),
-        pri_template.len() as CK_ULONG,
-        &mut prikey2,
-    );
-    assert_eq!(ret, CKR_OK);
-
-    assert_eq!(check_validation(session, 1), true);
-
-    /* Test the unwrapped key can be used */
-    let data = "plaintext";
-    let sig = ret_or_panic!(sig_gen(
-        session,
-        prikey2,
-        data.as_bytes(),
-        &CK_MECHANISM {
-            mechanism: CKM_ECDSA_SHA256,
-            pParameter: std::ptr::null_mut(),
-            ulParameterLen: 0,
-        },
-    ));
-    assert_eq!(sig.len(), 96);
-
-    /* And signature verified by the original public key */
-    assert_eq!(
-        CKR_OK,
-        sig_verify(
+    for t in &testdata {
+        let (pubkey, prikey) = ret_or_panic!(generate_key_pair(
             session,
-            pubkey,
-            data.as_bytes(),
-            sig.as_slice(),
-            &CK_MECHANISM {
-                mechanism: CKM_ECDSA_SHA256,
-                pParameter: std::ptr::null_mut(),
-                ulParameterLen: 0,
-            },
-        )
-    );
+            t.key_gen,
+            &[(CKA_CLASS, CKO_PUBLIC_KEY), (CKA_KEY_TYPE, t.key_type),],
+            &[(CKA_EC_PARAMS, t.ec_params.as_slice())],
+            &[(t.op_flags.0, true)],
+            &[(CKA_CLASS, CKO_PRIVATE_KEY), (CKA_KEY_TYPE, t.key_type),],
+            &[],
+            &[
+                (CKA_PRIVATE, true),
+                (CKA_SENSITIVE, true),
+                (CKA_TOKEN, true),
+                (t.op_flags.1, true),
+                (CKA_EXTRACTABLE, true),
+            ],
+        ));
+
+        if t.op_flags.1 == CKA_SIGN {
+            let sig = ret_or_panic!(sig_gen(
+                session,
+                prikey,
+                data.as_bytes(),
+                &CK_MECHANISM {
+                    mechanism: t.op_alg,
+                    pParameter: std::ptr::null_mut(),
+                    ulParameterLen: 0,
+                },
+            ));
+            assert_eq!(sig.len(), t.out_size);
+
+            assert_eq!(
+                CKR_OK,
+                sig_verify(
+                    session,
+                    pubkey,
+                    data.as_bytes(),
+                    sig.as_slice(),
+                    &CK_MECHANISM {
+                        mechanism: t.op_alg,
+                        pParameter: std::ptr::null_mut(),
+                        ulParameterLen: 0,
+                    },
+                )
+            );
+        }
+
+        /* Wrap EC keys in AES */
+        let iv = [0xCCu8; 4];
+        let mut mechanism: CK_MECHANISM = CK_MECHANISM {
+            mechanism: CKM_AES_KEY_WRAP_KWP,
+            pParameter: void_ptr!(&iv),
+            ulParameterLen: iv.len() as CK_ULONG,
+        };
+
+        let mut wrapped = vec![0u8; 65536];
+        let mut wrapped_len = wrapped.len() as CK_ULONG;
+
+        let mut ret = fn_wrap_key(
+            session,
+            &mut mechanism,
+            wk_handle,
+            prikey,
+            wrapped.as_mut_ptr(),
+            &mut wrapped_len,
+        );
+        assert_eq!(ret, CKR_OK);
+
+        assert_eq!(check_validation(session, 1), true);
+
+        let mut pri_template = make_attr_template(
+            &[(CKA_CLASS, CKO_PRIVATE_KEY), (CKA_KEY_TYPE, t.key_type)],
+            &[],
+            &[
+                (CKA_PRIVATE, true),
+                (CKA_SENSITIVE, true),
+                (CKA_TOKEN, true),
+                (t.op_flags.1, true),
+                (CKA_EXTRACTABLE, true),
+            ],
+        );
+
+        let mut prikey2 = CK_INVALID_HANDLE;
+        ret = fn_unwrap_key(
+            session,
+            &mut mechanism,
+            wk_handle,
+            wrapped.as_mut_ptr(),
+            wrapped_len,
+            pri_template.as_mut_ptr(),
+            pri_template.len() as CK_ULONG,
+            &mut prikey2,
+        );
+        assert_eq!(ret, CKR_OK);
+
+        assert_eq!(check_validation(session, 1), true);
+
+        if t.op_flags.1 == CKA_SIGN {
+            /* Test the unwrapped key can be used */
+            let sig = ret_or_panic!(sig_gen(
+                session,
+                prikey2,
+                data.as_bytes(),
+                &CK_MECHANISM {
+                    mechanism: t.op_alg,
+                    pParameter: std::ptr::null_mut(),
+                    ulParameterLen: 0,
+                },
+            ));
+            assert_eq!(sig.len(), t.out_size);
+
+            /* And signature verified by the original public key */
+            assert_eq!(
+                CKR_OK,
+                sig_verify(
+                    session,
+                    pubkey,
+                    data.as_bytes(),
+                    sig.as_slice(),
+                    &CK_MECHANISM {
+                        mechanism: t.op_alg,
+                        pParameter: std::ptr::null_mut(),
+                        ulParameterLen: 0,
+                    },
+                )
+            );
+        }
+    }
+
+    /* the rest is done only for "ecdsa" */
+    #[cfg(not(feature = "ecdsa"))]
+    {
+        testtokn.finalize();
+        return;
+    }
 
     /* Test CKA_ALWAYS_AUTHENTICATE */
-
     let ec_params = hex::decode(
         "06052B81040022", // secp384r1
     )
@@ -623,70 +674,6 @@ fn test_ecc_key() {
         &mut siglen,
     );
     assert_eq!(ret, CKR_OK);
-
-    testtokn.finalize();
-}
-
-#[cfg(all(feature = "eddsa", not(feature = "fips")))]
-#[test]
-#[parallel]
-fn test_eddsa_key() {
-    let mut testtokn = TestToken::initialized("test_eddsa_key", None);
-    let session = testtokn.get_session(true);
-
-    /* login */
-    testtokn.login();
-
-    /* Ed25519 key pair */
-    let ec_params = hex::decode(
-        "130c656477617264733235353139", // edwards25519
-    )
-    .expect("Failed to decode hex ec_params");
-
-    let (pubkey, prikey) = ret_or_panic!(generate_key_pair(
-        session,
-        CKM_EC_EDWARDS_KEY_PAIR_GEN,
-        &[(CKA_CLASS, CKO_PUBLIC_KEY), (CKA_KEY_TYPE, CKK_EC_EDWARDS),],
-        &[(CKA_EC_PARAMS, ec_params.as_slice())],
-        &[(CKA_VERIFY, true)],
-        &[(CKA_CLASS, CKO_PRIVATE_KEY), (CKA_KEY_TYPE, CKK_EC_EDWARDS),],
-        &[],
-        &[
-            (CKA_PRIVATE, true),
-            (CKA_SENSITIVE, true),
-            (CKA_TOKEN, true),
-            (CKA_SIGN, true),
-            (CKA_EXTRACTABLE, true),
-        ],
-    ));
-
-    let data = "plaintext";
-    let sig = ret_or_panic!(sig_gen(
-        session,
-        prikey,
-        data.as_bytes(),
-        &CK_MECHANISM {
-            mechanism: CKM_EDDSA,
-            pParameter: std::ptr::null_mut(),
-            ulParameterLen: 0,
-        },
-    ));
-    assert_eq!(sig.len(), 64);
-
-    assert_eq!(
-        CKR_OK,
-        sig_verify(
-            session,
-            pubkey,
-            data.as_bytes(),
-            sig.as_slice(),
-            &CK_MECHANISM {
-                mechanism: CKM_EDDSA,
-                pParameter: std::ptr::null_mut(),
-                ulParameterLen: 0,
-            },
-        )
-    );
 
     testtokn.finalize();
 }
