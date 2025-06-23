e27faf0ea4ef1ee970c7a92edfe07a440acd6829
Simo Sorce <simo@redhat.com>
2025-06-23 18:29:29 -0400

    Remove unused helpers from ossl/common.rs
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- src/ossl/common.rs
+++ src/ossl/common.rs
@@ -13,7 +13,7 @@ use crate::kasn1::oid;
 use crate::misc::*;
 use crate::object::Object;
 
-use ossl::bindings::*;
+use ossl::bindings::OPENSSL_cleanse;
 use ossl::digest::DigestAlg;
 use ossl::pkey::{EvpPkey, EvpPkeyType};
 use ossl::OsslContext;
@@ -93,18 +93,6 @@ pub const CIPHER_NAME_AES128: &[u8; 7] = b"AES128\0";
 pub const CIPHER_NAME_AES192: &[u8; 7] = b"AES192\0";
 pub const CIPHER_NAME_AES256: &[u8; 7] = b"AES256\0";
 
-/// Helper to turn 0 terminated slices, as constructed by bindgen for OpenSSL
-/// macro defined string into a CStr. Converts an error into a general error.
-macro_rules! cstr {
-    ($str:expr) => {
-        match ::std::ffi::CStr::from_bytes_with_nul($str) {
-            Ok(s) => s,
-            Err(e) => return Err(crate::error::Error::other_error(e)),
-        }
-    };
-}
-pub(crate) use cstr;
-
 /// Maps a PKCS#11 mechanism type involving a hash to the corresponding
 /// ossl DigestAlg
 pub fn mech_type_to_digest_alg(mech: CK_MECHANISM_TYPE) -> Result<DigestAlg> {
@@ -168,76 +156,6 @@ pub fn mech_type_to_digest_alg(mech: CK_MECHANISM_TYPE) -> Result<DigestAlg> {
         }
         CKM_SHA512_256_HMAC | CKM_SHA512_256_HMAC_GENERAL | CKM_SHA512_256 => {
             DigestAlg::Sha2_512_256
-        }
-        _ => return Err(CKR_MECHANISM_INVALID)?,
-    })
-}
-
-/// Maps a PKCS#11 mechanism type involving a hash to the corresponding
-/// OpenSSL digest name string (e.g., `CKM_SHA256_RSA_PKCS` -> `"SHA256"`).
-pub fn mech_type_to_digest_name(
-    mech: CK_MECHANISM_TYPE,
-) -> Result<&'static CStr> {
-    Ok(match mech {
-        CKM_SHA1_RSA_PKCS
-        | CKM_ECDSA_SHA1
-        | CKM_SHA1_RSA_PKCS_PSS
-        | CKM_SHA_1_HMAC
-        | CKM_SHA_1_HMAC_GENERAL
-        | CKM_SHA_1 => cstr!(OSSL_DIGEST_NAME_SHA1),
-        CKM_SHA224_RSA_PKCS
-        | CKM_ECDSA_SHA224
-        | CKM_SHA224_RSA_PKCS_PSS
-        | CKM_SHA224_HMAC
-        | CKM_SHA224_HMAC_GENERAL
-        | CKM_SHA224 => cstr!(OSSL_DIGEST_NAME_SHA2_224),
-        CKM_SHA256_RSA_PKCS
-        | CKM_ECDSA_SHA256
-        | CKM_SHA256_RSA_PKCS_PSS
-        | CKM_SHA256_HMAC
-        | CKM_SHA256_HMAC_GENERAL
-        | CKM_SHA256 => cstr!(OSSL_DIGEST_NAME_SHA2_256),
-        CKM_SHA384_RSA_PKCS
-        | CKM_ECDSA_SHA384
-        | CKM_SHA384_RSA_PKCS_PSS
-        | CKM_SHA384_HMAC
-        | CKM_SHA384_HMAC_GENERAL
-        | CKM_SHA384 => cstr!(OSSL_DIGEST_NAME_SHA2_384),
-        CKM_SHA512_RSA_PKCS
-        | CKM_ECDSA_SHA512
-        | CKM_SHA512_RSA_PKCS_PSS
-        | CKM_SHA512_HMAC
-        | CKM_SHA512_HMAC_GENERAL
-        | CKM_SHA512 => cstr!(OSSL_DIGEST_NAME_SHA2_512),
-        CKM_SHA3_224_RSA_PKCS
-        | CKM_ECDSA_SHA3_224
-        | CKM_SHA3_224_RSA_PKCS_PSS
-        | CKM_SHA3_224_HMAC
-        | CKM_SHA3_224_HMAC_GENERAL
-        | CKM_SHA3_224 => cstr!(OSSL_DIGEST_NAME_SHA3_224),
-        CKM_SHA3_256_RSA_PKCS
-        | CKM_ECDSA_SHA3_256
-        | CKM_SHA3_256_RSA_PKCS_PSS
-        | CKM_SHA3_256_HMAC
-        | CKM_SHA3_256_HMAC_GENERAL
-        | CKM_SHA3_256 => cstr!(OSSL_DIGEST_NAME_SHA3_256),
-        CKM_SHA3_384_RSA_PKCS
-        | CKM_ECDSA_SHA3_384
-        | CKM_SHA3_384_RSA_PKCS_PSS
-        | CKM_SHA3_384_HMAC
-        | CKM_SHA3_384_HMAC_GENERAL
-        | CKM_SHA3_384 => cstr!(OSSL_DIGEST_NAME_SHA3_384),
-        CKM_SHA3_512_RSA_PKCS
-        | CKM_ECDSA_SHA3_512
-        | CKM_SHA3_512_RSA_PKCS_PSS
-        | CKM_SHA3_512_HMAC
-        | CKM_SHA3_512_HMAC_GENERAL
-        | CKM_SHA3_512 => cstr!(OSSL_DIGEST_NAME_SHA3_512),
-        CKM_SHA512_224_HMAC | CKM_SHA512_224_HMAC_GENERAL | CKM_SHA512_224 => {
-            cstr!(OSSL_DIGEST_NAME_SHA2_512_224)
-        }
-        CKM_SHA512_256_HMAC | CKM_SHA512_256_HMAC_GENERAL | CKM_SHA512_256 => {
-            cstr!(OSSL_DIGEST_NAME_SHA2_512_256)
         }
         _ => return Err(CKR_MECHANISM_INVALID)?,
     })
