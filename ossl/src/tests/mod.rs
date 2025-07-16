216a9acec397fd057487203007a1249f0ece0718
Simo Sorce <simo@redhat.com>
2025-07-16 10:18:14 -0400

    Handle issues in ossl tests with FIPS builds
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- ossl/src/tests/mod.rs
+++ ossl/src/tests/mod.rs
@@ -1,4 +1,20 @@
 // Copyright 2025 Simo Sorce
 // See LICENSE.txt file for terms
 
+#[cfg(not(feature = "fips"))]
+static TEST_CONTEXT: ::std::sync::LazyLock<crate::OsslContext> =
+    ::std::sync::LazyLock::new(|| crate::OsslContext::new_lib_ctx());
+
+pub fn test_ossl_context() -> &'static crate::OsslContext {
+    #[cfg(feature = "fips")]
+    {
+        crate::fips::get_libctx()
+    }
+    #[cfg(not(feature = "fips"))]
+    {
+        &TEST_CONTEXT
+    }
+}
+
+#[cfg(not(feature = "fips"))]
 mod brainpool;
