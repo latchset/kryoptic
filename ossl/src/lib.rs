6160288e6c0d279a2eae933645f8c1b65e6529cf
Simo Sorce <simo@redhat.com>
2025-07-28 09:58:40 -0400

    Silence unnecessary transmute error
    At least until bindgen is updated to do the right thing
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- ossl/src/lib.rs
+++ ossl/src/lib.rs
@@ -13,6 +13,7 @@ pub mod bindings {
     #![allow(non_camel_case_types)]
     #![allow(dead_code)]
     #![allow(non_snake_case)]
+    #![allow(unnecessary_transmutes)]
     include!(concat!(env!("OUT_DIR"), "/ossl_bindings.rs"));
 }
 
