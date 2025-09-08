833c38521f02f61952068aaea1882dcd4b486c84
Jakub Jelen <jjelen@redhat.com>
2025-09-08 16:43:01 +0200

    Override wrong SHA3 OIDs from pyca
    Upstream issue:
    https://github.com/pyca/cryptography/issues/13331
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/kasn1/oid.rs
+++ src/kasn1/oid.rs
@@ -23,3 +23,14 @@ pub const KKDF1_OID: asn1::ObjectIdentifier =
 /// Kryoptic Key Based Protection Scheme v1
 pub const KKBPS1_OID: asn1::ObjectIdentifier =
     asn1::oid!(1, 3, 6, 1, 4, 1, 2312, 20, 2, 1);
+
+// The SHA3 OIDs are wrong in pyca
+// https://github.com/pyca/cryptography/issues/13331
+pub const SHA3_224_OID: asn1::ObjectIdentifier =
+    asn1::oid!(2, 16, 840, 1, 101, 3, 4, 2, 7);
+pub const SHA3_256_OID: asn1::ObjectIdentifier =
+    asn1::oid!(2, 16, 840, 1, 101, 3, 4, 2, 8);
+pub const SHA3_384_OID: asn1::ObjectIdentifier =
+    asn1::oid!(2, 16, 840, 1, 101, 3, 4, 2, 9);
+pub const SHA3_512_OID: asn1::ObjectIdentifier =
+    asn1::oid!(2, 16, 840, 1, 101, 3, 4, 2, 10);
